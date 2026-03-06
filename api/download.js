e70692306cfce05f89b688c5897bf641891ef67c
Fahz Kang Rinem <fahrifakhrullah@gmail.com>
2026-03-06 15:01:16 +0700

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,1 +1,37 @@
+import axios from "axios"
 
+export default async function handler(req,res){
+
+const API_KEY="ISI_API_KEY_KAMU"
+
+const API_BASE="https://back.asitha.top/api/ytapi"
+
+const {url}=req.body
+
+try{
+
+const response=await axios.get(API_BASE,{
+params:{
+url:url,
+fo:"mp3",
+qu:"128"
+},
+headers:{
+Authorization:`Bearer ${API_KEY}`
+}
+})
+
+res.status(200).json({
+status:true,
+link:response.data.link
+})
+
+}catch(err){
+
+res.status(500).json({
+status:false
+})
+
+}
+
+}
